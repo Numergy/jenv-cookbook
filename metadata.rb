4bb5c9232bce2b167a58b0583d6cc8bfebc55ac1
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2015-01-14 14:14:12 +0100

    Bump to 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'khai-wen.tran@numergy.com'
 license 'All rights reserved'
 description 'Installs/Configures jenv'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.3.0'
 
 depends 'apt'
 depends 'git'
